e2282f53f900890e7ce9f2b0931b598ab706bd8e
mmwandera <mmwandera74@gmail.com>
2023-11-19 19:33:31 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -15,16 +15,23 @@ function Home() {
     </div>
   );
 }
+// Create a third component, <About>. The <About> component should also be a child component of <App>.
+// The <About> component should return a <div> with an id of about. To match the screenshot, the div should contain an <h2> element with the text "About." However, as long as it has an id of about, it will pass the test!
+function About(){
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
-{/* write an <About> component here */}
-
-
+// Two components, <Navbar> and <Home>, are already defined in the App.js file. Display both of these components as child components of <App>.
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
